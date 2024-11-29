71cc86047fe8332bd5de55378f4f1a5e6d285384
vishal pandhare <vishal@cowhite.com>
2024-11-29 13:41:57 +0530

    railway url added

--- src/const/baseurl.js
+++ src/const/baseurl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://portfolio-be-2-i5k7.onrender.com"
+export const baseUrl = "https://portfoliobe-production-5013.up.railway.app/"
 export const portfolioUrl = "https://nine-portfolio.vercel.app/"
