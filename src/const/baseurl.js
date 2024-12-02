05624a2fba2c496ab6433e4d158da7c9e1bf1eed
Vishal Pandhare <99859675+vishalpandhare01@users.noreply.github.com>
2024-12-02 13:59:13 +0530

    Update baseurl.js

--- src/const/baseurl.js
+++ src/const/baseurl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://portfoliobe-production-5013.up.railway.app/"
+export const baseUrl = "https://portfoliobe-production-5013.up.railway.app"
 export const portfolioUrl = "https://nine-portfolio.vercel.app/"
